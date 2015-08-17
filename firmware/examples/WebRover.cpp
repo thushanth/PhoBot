c03c690884639fdaedff5ca931a612fb30f2fa37
Thushanth <thushanth@hotmail.com>
2015-08-17 10:21:30 -0400

    Update WebRover.cpp

--- firmware/examples/WebRover.cpp
+++ firmware/examples/WebRover.cpp
@@ -1,10 +1,7 @@
 /* 
-
 This is an example program for the Photon_Bot library for the
 Robot controller of the same name, by MonkMakes.com
-
 You will need to include the HC_SR04 library
-
 */
 
 #include "PhoBot-ABRA/PhoBot-ABRA.h"
@@ -14,13 +11,16 @@ double volts = 0.0;
 double distance = 0.0;
 
 
-PhoBot p = PhoBot();
+PhoBot p = PhoBot(6.0, 3.0); //assuming you are using ABRA kit.
 HC_SR04 rangefinder = HC_SR04(p.trigPin, p.echoPin);
 
 void setup() {
     Spark.function("control", control);
     Spark.variable("volts", &volts, DOUBLE);
     Spark.variable("distance", &distance, DOUBLE);
+    pinMode(A3, OUTPUT);
+    pinMode(A4, OUTPUT);
+    pinMode(A5, OUTPUT);
 }
 
 void loop() {
@@ -29,5 +29,16 @@ void loop() {
 }
 
 int control(String command) {
+    if(command == "M3-S" || command == "M4-S")
+    {
+        p.setMotors(command);
+        digitalWrite(A3, LOW);
+    }
+    else
+    {
+        p.setMotors(command);
+        digitalWrite(A3, HIGH);
+    }
+    p.setMotors(command);
     return p.control(command);
 }
